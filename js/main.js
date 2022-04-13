6a2efe3910031e79c4c0caaeacf405e47035711f
cookie.729 <cookie.729@icloud.com>
2022-04-14 01:04:14 +0900

    ecsite.fam-fan

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 'use strict'
+
+document.getElementById('search-form').onsubmit = function(event) {
+  event.preventDefault();
+  const search = document.getElementById('search-form').word.value;
+  document.getElementById('search-output').textContent =`「${search}」の検索中・・・`;
+};
+
+const btn = document.querySelector('.btn-menu');
+const nav = document.querySelector('.main-nav');
+btn.addEventListener('click', () => {
+  nav.classList.toggle('open-menu');
+  if (btn.innerHTML === '<i class="fa-solid fa-bars"></i>') {
+    btn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  } else {
+    btn.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  }
+});
